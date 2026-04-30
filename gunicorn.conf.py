a94d47f708193793f1f3ebcbd24061e1cb84a25d
Nathan Barrett <nbarrett@mozilla.com>
2026-04-30 13:04:03 -0500

    fix: fix errors in argocd deployment
    * fix: move control path to /tmp/ to fix error in argocd
    
    * Update workflow to look for latest

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,6 +4,7 @@ bind = "0.0.0.0:8000"
 workers = int(os.environ.get("GUNICORN_WORKERS", 4))
 worker_class = "sync"
 worker_tmp_dir = "/dev/shm"
+control_socket_disable = True
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
