991d9a50596ca2a67c012dc1323d8f888aabb27d
Nathan Barrett <nbarrett@mozilla.com>
2026-05-01 10:22:20 -0500

    fix: preload app in gunicorn to speed up worker startup

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,6 +3,7 @@ import os
 bind = "0.0.0.0:8000"
 workers = int(os.environ.get("GUNICORN_WORKERS", 4))
 worker_class = "sync"
+preload_app = True
 worker_tmp_dir = "/dev/shm"
 control_socket_disable = True
 accesslog = "-"
