d19da3b1e71153530c8e43ce704f70493e658bfd
Nathan Barrett <nbarrett@mozilla.com>
2026-06-11 17:57:50 -0500

    Bug 2037238 - Add post_fork hook  (#49)

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,9 @@
 import os
 
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 bind = "0.0.0.0:8000"
 workers = int(os.environ.get("GUNICORN_WORKERS", 4))
 worker_class = "sync"
@@ -8,3 +12,11 @@ control_socket_disable = True
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
+
+
+def post_fork(server, worker):
+    # BatchSpanProcessor's export thread isn't fork-safe, re-add one per worker so
+    # request spans actually flush. https://opentelemetry-python.readthedocs.io/en/stable/examples/fork-process-model/README.html
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(OTLPSpanExporter())
+    )
