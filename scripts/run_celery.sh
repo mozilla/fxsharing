c3f5ecf0e97a56f571e71a6466fe7e70204191a0
Nathan Barrett <nbarrett@mozilla.com>
2026-06-15 10:00:35 -0500

    Update otel to auto-instumentation (#53)

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -2,4 +2,4 @@
 
 CELERY_NUM_WORKERS=${CELERY_NUM_WORKERS:-"2"}
 
-opentelemetry-instrument celery -A fxsharing worker --concurrency=$CELERY_NUM_WORKERS --loglevel=info
+exec celery -A fxsharing worker --concurrency=$CELERY_NUM_WORKERS --loglevel=info
